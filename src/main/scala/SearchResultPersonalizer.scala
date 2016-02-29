57bcdfad032790089b1c4a8fe0a9752ccedfb087
santacos <santacos_lala@hotmail.com>
2016-02-29 16:42:14 +0700

    modify personalize function : return top-10 recommend

--- src/main/scala/SearchResultPersonalizer.scala
+++ src/main/scala/SearchResultPersonalizer.scala
@@ -1,8 +1,14 @@
+import org.apache.spark.mllib.recommendation.MatrixFactorizationModel
 import spark.context.RankingContext._
 class SearchResultPersonalizer {
 
   def personalize(nonPersonalizedSearchResult: NonPersonalizedSearchResult): PersonalizedSearchResult = {
     //PersonalizedSearchResult(nonPersonalizedSearchResult.items)
-    sparkContext
+
+    val explicitModel = MatrixFactorizationModel.load(sparkContext,modelDir)
+    val recommendProduct = explicitModel.recommendProducts(nonPersonalizedSearchResult.user,10)
+    PersonalizedSearchResult(recommendProduct.map(_.product).toList)
+
+
   }
 }
--- src/main/scala/spark/context/RankingContext.scala
+++ src/main/scala/spark/context/RankingContext.scala
@@ -6,6 +6,14 @@ import org.apache.spark.{SparkConf, SparkContext}
   * Created by ibosz on 29/2/59.
   */
 object RankingContext {
-  private val config = ???
+  private val config = new SparkConf()
+    .setMaster("local[4]")
+    .setAppName("Sigmoid Spark")
+    .set("spark.executor.memory", "2g")
+    .set("spark.rdd.compress", "true")
+    .set("spark.storage.memoryFraction", "0.5")
+
   lazy val sparkContext = new SparkContext(config)
+
+  val modelDir = "/Users/santacos/senior_file/Explicit/model05"
 }
