0f6cadfe54db0b107173327237bb0fce014976bc
Supanat Potiwarakorn <supanat.ptk@gmail.com>
2016-03-24 14:59:20 +0700

    write tests for  RDDStringToDataFrame

---
+++ src/main/scala/wongnai/mlservice/preprocess/RDDStringToDataFrameConstructor.scala
@@ -0,0 +1,28 @@
+package wongnai.mlservice.preprocess
+
+import org.apache.spark.rdd.RDD
+import org.apache.spark.sql.{DataFrame, SQLContext}
+
+import scala.reflect.ClassTag
+import scala.reflect.runtime.universe.TypeTag
+import scala.util.matching.Regex
+/**
+  * Created by ibosz on 22/3/59.
+  */
+class RDDStringToDataFrameConstructor[ColumnTypeInTuple <: Product :TypeTag:ClassTag](
+    val pattern: Regex,
+    val outputCols: Seq[String],
+    val capturedListToTuple: List[String] => ColumnTypeInTuple
+  )(implicit constructionContext: SQLContext) {
+
+  def createDataFrame(rdd: RDD[String]): DataFrame = {
+    import constructionContext.implicits._
+    val reg = pattern
+    val toTuple = capturedListToTuple
+
+    rdd
+      .map(reg.unapplySeq(_) match {
+        case Some(list) => toTuple(list) })
+      .toDF(outputCols: _*)
+  }
+}
---
+++ src/test/wongnai/mlservice/preprocess/RDDStringToDataFrameConstructorTest.scala
@@ -0,0 +1,87 @@
+package wongnai.mlservice.preprocess
+
+import org.apache.spark.sql.Row
+import org.scalatest.Matchers
+import testutil.FunSuiteSpark
+
+import scala.util.matching.Regex
+
+/**
+  * Created by ibosz on 22/3/59.
+  */
+class RDDStringToDataFrameConstructorTest extends FunSuiteSpark with Matchers {
+  import testutil.FunSuiteSpark._
+
+  test("DataFrame columns' name should be as it is predefined") {
+    implicit val dataFrameConstructionContext = sqlContext
+
+    val toTuple = (listOfMatched: List[String]) => listOfMatched match {
+      case List(user, item, score) => (user, item, score)
+    }
+    val pattern = new Regex("(\\w)(\\w)(\\w)")
+    val columns = Seq("user", "item", "score")
+
+    val dataFrameConstructor =
+      new RDDStringToDataFrameConstructor
+        [(String, String, String)](pattern, columns, toTuple)
+
+    val rdd = sparkContext.parallelize(List("aaa", "bbb", "ccc"))
+    val dataFrame = dataFrameConstructor.createDataFrame(rdd)
+
+    dataFrame.schema.fieldNames shouldBe columns.toArray
+  }
+
+  test("type of each column should convert correctly") {
+    implicit val dataFrameConstructionContext = sqlContext
+
+    val toTuple = (listOfMatched: List[String]) => listOfMatched match {
+      case List(user, item, score) => (user.toInt, item.toInt, score.toDouble)
+    }
+    val pattern = new Regex("(\\d+)::(\\d+)::(\\d*.\\d+)")
+    val columns = Seq("user", "item", "score")
+
+    val dataFrameConstructor =
+      new RDDStringToDataFrameConstructor
+        [(Int, Int, Double)](pattern, columns, toTuple)
+
+    val rdd = sparkContext.parallelize(List(
+      "1::2::2.4", "2::4::5.6", "5::6::1.2"
+    ))
+
+    val dataFrame = dataFrameConstructor.createDataFrame(rdd)
+
+    dataFrame.collect().foreach(row => row match {
+      case Row(user, item, score) =>
+        user.getClass.getName shouldBe "java.lang.Integer"
+        item.getClass.getName shouldBe "java.lang.Integer"
+        score.getClass.getName shouldBe "java.lang.Double"
+    })
+  }
+
+  test("create DataFrame correctly") {
+    implicit val dataFrameConstructionContext = sqlContext
+
+    val toTuple = (listOfMatched: List[String]) => listOfMatched match {
+      case List(user, item, score) => (user.toInt, item.toInt, score.toDouble)
+    }
+    val pattern = new Regex("(\\d+)::(\\d+)::(\\d*.\\d+)")
+    val columns = Seq("user", "item", "score")
+
+    val dataFrameConstructor =
+      new RDDStringToDataFrameConstructor
+        [(Int, Int, Double)](pattern, columns, toTuple)
+
+    val rdd = sparkContext.parallelize(List(
+      "1::2::2.4", "2::4::5.6", "5::6::1.2"
+    ))
+
+    val dataFrame = dataFrameConstructor.createDataFrame(rdd)
+
+    dataFrame.collect() should (
+      contain (Row(1, 2, 2.4)) and
+      contain (Row(2, 4, 5.6)) and
+      contain (Row(5, 6, 1.2))
+    )
+  }
+
+}
